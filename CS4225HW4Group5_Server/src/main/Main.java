a617c410b379415d9cdf6c4a9ec3ace041b5225a
jfulle11 <jfulle11@my.westga.edu>
2020-02-26 23:34:00 -0500

    matrix prototype and simplifications added

--- CS4225HW4Group5_Server/src/main/Main.java
+++ CS4225HW4Group5_Server/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
 
 import matrix.Matrix;
@@ -9,18 +10,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Matrix m0 = new Matrix();
-		m0.readFromFile("matrix1.txt");
-		System.out.println(m0.getString());
+		Matrix m0 = new Matrix(new File("matrix1.txt"));
+		System.out.println(m0.stringify());
 		
-		Matrix m1 = new Matrix();
 		try {
-			m0.readFromString(new String(FileUtils.readFile("matrix2.txt")));
+			Matrix m1 = new Matrix(new String(FileUtils.readFile("matrix2.txt")));
+			System.out.println(m1.stringify());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(m1.getString());
+		
 		
 	}
 
--- CS4225HW4Group5_Server/src/matrix/Matrix.java
+++ CS4225HW4Group5_Server/src/matrix/Matrix.java
@@ -1,5 +1,6 @@
 package matrix;
 
+import java.io.File;
 import java.io.IOException;
 
 import utils.FileUtils;
@@ -12,7 +13,69 @@ public class Matrix {
 		this.matrix = null;
 	}
 	
-	public void readFromFile(String filePath) {
+	public Matrix(File file) {
+		this.readFromFile(file.getPath());
+	}
+	
+	public Matrix(String rawMatrix) {
+		this.readFromString(rawMatrix);
+	}
+	
+	
+	public Matrix(int height, int width) {
+		this.matrix = new double[height][width];
+	}
+	
+	public Matrix multiply(Matrix b) {
+		
+		if(b.getHeight() == -1 || b.getWidth() != this.getHeight()) {
+			return null;
+		}
+		
+		Matrix eval = new Matrix(b.getWidth(), this.getHeight());
+		
+		return null;
+	}
+	
+	public int getWidth() {
+		return this.matrix == null? -1 : this.matrix[0].length;
+	}
+	
+	public int getHeight() {
+		return this.matrix == null? -1 : this.matrix.length;
+	}
+	
+	public String stringify() {
+		
+		if(this.matrix == null) {
+			return "[]";
+		}
+		
+		StringBuilder build = new StringBuilder();
+		build.append("[");
+		for(int y = 0; y < this.matrix.length; y++) {
+			for(int x = 0; x < this.matrix[y].length; x++) {
+				
+				build.append(this.matrix[y][x]);
+				
+				if(x!=this.matrix[y].length-1) {
+					build.append(", ");
+				}
+			}
+			
+			if(y!=this.matrix.length-1) {
+				build.append("\n");
+			}
+		}
+		build.append("]");
+		
+		return build.toString();
+	}
+	
+	
+	
+	
+	private void readFromFile(String filePath) {
 		try {
 			
 			String[] values = FileUtils.readCsv(filePath);
@@ -24,7 +87,7 @@ public class Matrix {
 		}
 	}
 	
-	public void readFromString(String rawMatrix) {
+	private void readFromString(String rawMatrix) {
 		
 		rawMatrix = FileUtils.condenseNewLines(rawMatrix);
 		rawMatrix = rawMatrix.replaceAll("\n", ",");
@@ -41,38 +104,19 @@ public class Matrix {
 		int height = Integer.parseInt(pieces[0].trim());
 		int width = Integer.parseInt(pieces[1].trim());
 		
-		this.matrix = new double[width][height];
+		this.matrix = new double[height][width];
 		
-		if((pieces.length-1)%width != 0) {
-			//error sparse data
+		if((csvValues.length-1)%width != 0) {
+			///TODO error sparse data maybe handle case
 			return;
 		}
 		
-		for(int y = 0; y < this.matrix.length; y++) {
-			for(int x = 0; x < this.matrix[y].length; x++) {
-				this.matrix[x][y] = Double.parseDouble(csvValues[x + y*width +1]);
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				this.matrix[y][x] = Double.parseDouble(csvValues[x + y*width +1].trim());
 			}
 		}
 		
-	}
-	
-	public String getString() {
-		
-		StringBuilder build = new StringBuilder();
-		build.append("[");
-		for(int y = 0; y < this.matrix.length; y++) {
-			for(int x = 0; x < this.matrix[y].length; x++) {
-				build.append(this.matrix[y][x]);
-				if(x!=this.matrix[y].length-1) {
-					build.append(", ");
-				}
-			}
-			if(y!=this.matrix.length-1) {
-				build.append("\n");
-			}
-		}
-		build.append("]");
-		return build.toString();
 	}
 	
 }
